f9f0ccd77d6e1c607a6b411ecf0ed7c499e6f4c1
Juan Carlos Rubiano P <juan.rubiano@certicamara.com>
2017-07-24 11:33:56 -0500

    First commit

--- src/main/scala/edu/uniandes/Hello.scala
+++ src/main/scala/edu/uniandes/Hello.scala
@@ -1,4 +1,4 @@
-package example
+package edu.uniandes
 
 object Hello extends Greeting with App {
   println(greeting)
--- src/test/scala/edu/uniandes/HelloSpec.scala
+++ src/test/scala/edu/uniandes/HelloSpec.scala
@@ -1,4 +1,4 @@
-package example
+package edu.uniandes
 
 import org.scalatest._
 
