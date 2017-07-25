2c417b2e01a414af7179940aca20bfc3212c41b4
Juan Carlos Rubiano P <juan.rubiano@certicamara.com>
2017-07-24 19:06:31 -0500

    Insert en tabla, primera versión

--- src/main/scala/edu/uniandes/Dominio/EstructuraDeDatos.scala
+++ src/main/scala/edu/uniandes/Dominio/EstructuraDeDatos.scala
@@ -1,5 +1,13 @@
 package edu.uniandes.Dominio
 
 trait Dato
+
 case class TipoDeDato[T] (nombreCampo: String,valor: T) extends Dato
-case class TablaItems(registros: List[List[Dato]])
+
+case class TablaItems(registros: List[List[Dato]]){
+  var registrosTabla = registros
+  def insertInto(reg: List[List[Dato]]): TablaItems ={
+    registrosTabla = this.registrosTabla ::: reg
+    this
+  }
+}
--- src/main/scala/edu/uniandes/Hello.scala
+++ src/main/scala/edu/uniandes/Hello.scala
@@ -3,33 +3,36 @@ package edu.uniandes
 import edu.uniandes.Dominio.{Dato, TablaItems, TipoDeDato}
 
 object Hello extends App {
-  val lista: Seq[TipoDeDato[String]] = List(
-    new TipoDeDato[String]("items", "Revistas"),
-    new TipoDeDato[String]("items", "Libros digitales"),
-    new TipoDeDato[String]("items", "Revistas digitales"),
-    new TipoDeDato[String]("items", "Libros")
+
+  val registro0: List[Dato] = List(
+    new TipoDeDato[String]("Autor", "HGabo"),
+    new TipoDeDato[String]("Titulo", "ZLa María"),
+    new TipoDeDato[Int]("Numero de Paginas", 752)
   )
-  lista.sortBy(r => r.valor).foreach(println)
 
   val registro: List[Dato] = List(
-    new TipoDeDato[String]("Autor", "Gabo"),
-    new TipoDeDato[String]("Titulo", "La María"),
+    new TipoDeDato[String]("Autor", "HGabo"),
+    new TipoDeDato[String]("Titulo", "ZLa María"),
     new TipoDeDato[Int]("Numero de Paginas", 752)
   )
 
   val registro1: List[Dato] = List(
-    new TipoDeDato[String]("Autor", "Gabo"),
-    new TipoDeDato[String]("Titulo", "La María"),
+    new TipoDeDato[String]("Autor", "BGabo"),
+    new TipoDeDato[String]("Titulo", "YLa María"),
     new TipoDeDato[Int]("Numero de Paginas", 752)
   )
 
   val registro2: List[Dato] = List(
-    new TipoDeDato[String]("Autor", "Gabo"),
-    new TipoDeDato[String]("Titulo", "La María"),
+    new TipoDeDato[String]("Autor", "AGabo"),
+    new TipoDeDato[String]("Titulo", "XLa María"),
     new TipoDeDato[Int]("Numero de Paginas", 752)
   )
 
-  val Registros: List[List[Dato]] = List(registro,registro1,registro2)
+  val Registros1: List[List[Dato]] = List(registro0)
+  val Registros2: List[List[Dato]] = List(registro,registro1,registro2)
 
-  val tablaLibros = new TablaItems(Registros)
+  val tablaLibros: TablaItems = new TablaItems(Registros1)
+  tablaLibros.registrosTabla.foreach(println)
+  println("----")
+  tablaLibros.insertInto(Registros2).registrosTabla.foreach(println)
 }
