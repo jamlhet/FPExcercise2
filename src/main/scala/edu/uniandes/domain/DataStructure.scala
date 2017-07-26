3b637caec07f3ea01194d6d88e57b1bbd9ffa850
Juan Carlos Rubiano Panadero <jamlhet@gmail.com>
2017-07-26 00:25:22 -0500

    Second version for SQL Syntax

--- src/main/scala/edu/uniandes/domain/DataStructure.scala
+++ src/main/scala/edu/uniandes/domain/DataStructure.scala
@@ -1,27 +1,17 @@
 package edu.uniandes.domain
 
-import scala.math.Ordered.orderingToOrdered
-
 sealed trait DataType
 
 case class Author(autor:String) extends DataType with Ordered[Author]{
-  override def compare(that: Author): Int = (this.autor) compare (that.autor)
+  override def compare(that: Author): Int = this.autor compare that.autor
 }
 
 case class Title(title:String) extends DataType with Ordered[Title]{
-  override def compare(that: Title): Int = (this.title) compare (that.title)
+  override def compare(that: Title): Int = this.title compare that.title
 }
 
 case class Pages(pages:Int) extends DataType with Ordered[Pages]{
-  override def compare(that: Pages): Int = (this.pages) compare (that.pages)
+  override def compare(that: Pages): Int = this.pages compare that.pages
 }
 
 case class Book(author: Author, title:Title, pages:Pages)
-
-class ItemsTable(var libros: List[List[DataType]]){
-  def insertInto(newBook: List[List[DataType]]): ItemsTable ={
-    libros = libros ::: newBook
-    this
-  }
-}
-
