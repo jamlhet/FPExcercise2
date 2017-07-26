7cf2a2a621b9da38bc268f6ea8e949c5df79244f
Juan Carlos Rubiano Panadero <jamlhet@gmail.com>
2017-07-26 00:22:33 -0500

    Second version for SQL Syntax

--- src/main/scala/edu/uniandes/Library.scala
+++ src/main/scala/edu/uniandes/Library.scala
@@ -4,11 +4,11 @@ import edu.uniandes.domain._
 
 object Library extends App{
 
-  val book1 = new Book(Author("HGabo"),Title("ZLa María"),Pages(500))
-  val book2 = new Book(Author("ZGabo"),Title("RLa María"),Pages(100))
-  val book3 = new Book(Author("AGabo"),Title("XLa María"),Pages(10000))
-  val book4 = new Book(Author("XGabo"),Title("ALa María"),Pages(50))
-  val book5 = new Book(Author("XGabo"),Title("ALa María"),Pages(55))
+  val book1 = Book(Author("HGabo"),Title("ZLa María"),Pages(500))
+  val book2 = Book(Author("ZGabo"),Title("RLa María"),Pages(100))
+  val book3 = Book(Author("AGabo"),Title("XLa María"),Pages(10000))
+  val book4 = Book(Author("XGabo"),Title("ALa María"),Pages(50))
+  val book5 = Book(Author("XGabo"),Title("ALa María"),Pages(55))
 
   val booksTable = new LibrarySQL(List(book1,book2,book3))
   booksTable.insertInto(booksTable)(book4)
@@ -16,6 +16,5 @@ object Library extends App{
   booksTable.select(booksTable)(book4)
   booksTable.select(booksTable)(book5)
   booksTable.update(booksTable)(book4,book1)
-  println("+++++++++++++++++++++++++++++++")
   booksTable.update(booksTable)(book4,book5)
 }
