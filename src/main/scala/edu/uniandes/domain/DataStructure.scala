55e63fb57b06c686ed55fa593c46d082ec8bce4b
Juan Carlos Rubiano Panadero <jamlhet@gmail.com>
2017-07-28 00:18:29 -0500

    Third version for LibrarySQL Syntax

--- src/main/scala/edu/uniandes/domain/DataStructure.scala
+++ src/main/scala/edu/uniandes/domain/DataStructure.scala
@@ -1,19 +1,27 @@
 package edu.uniandes.domain
 
-abstract class DataType[T]() extends Ordered[T]{
+abstract class LibraryDataType[T]() extends Ordered[T] {
 
 }
 
-case class Author(author: String) extends DataType[Author] {
+case class Author(author: String) extends LibraryDataType[Author] {
   override def compare(that: Author): Int = this.author compare that.author
 }
 
-case class Title(title: String) extends DataType[Title] {
+case class Title(title: String) extends LibraryDataType[Title] {
   override def compare(that: Title): Int = this.title compare that.title
 }
 
-case class Pages(pages: Int) extends DataType[Pages] {
+case class Pages(pages: Int) extends LibraryDataType[Pages] {
   override def compare(that: Pages): Int = this.pages compare that.pages
 }
 
-case class Book(author: Author, title: Title, pages: Pages)
+case class IssueNumber(issueNumber: Int) extends LibraryDataType[IssueNumber] {
+  override def compare(that: IssueNumber): Int = this.issueNumber compare that.issueNumber
+}
+
+case class IsElectronic(isElectronic: Boolean) extends LibraryDataType[IsElectronic] {
+  override def compare(that: IsElectronic): Int = this.isElectronic compare that.isElectronic
+}
+
+case class LibraryItem(author: Author = Author("Anonymous"), title: Title = Title("No title"), pages: Pages = Pages(0), issueNumber: IssueNumber = IssueNumber(0), IsElectronic: Boolean = false)
--- src/main/scala/edu/uniandes/domain/LibrarySQL.scala
+++ src/main/scala/edu/uniandes/domain/LibrarySQL.scala
@@ -1,42 +1,83 @@
 package edu.uniandes.domain
 
 class LibrarySQL[T](var listRegisters: List[T]) {
+
   def insertInto(table: LibrarySQL[T])(newRegister: T): LibrarySQL[T] = {
     println(s"Insert process...")
     if (findOne(table)(newRegister).size > 1) {
       println(s"The item already exists in the database")
-      this
+      table
     } else {
-      this.listRegisters = this.listRegisters ::: List(newRegister)
-      println(s"The item ${newRegister} was inserted successfully")
-      this
+      table.listRegisters = table.listRegisters ::: List(newRegister)
+      println(s"The item $newRegister was inserted successfully")
+      table
     }
   }
 
-  def select(table: LibrarySQL[T])(where: Any): List[T] = {
+  def select(table: LibrarySQL[T])(where: List[_ >: String with Any]): List[T] = {
     println("Select process...")
-    where match {
-      case where: String =>
-        if (where equals "*") {
-          println(s"Records found ${listRegisters.size}")
-          listRegisters.foreach(x => println(x))
-          listRegisters
-        } else {
-          List()
+    where.head match {
+      case "*" =>
+        println(s"Records found ${table.listRegisters.size}")
+        table.listRegisters.foreach(x => println(x))
+        table.listRegisters
+      case ">" =>
+        where.last match {
+          case Author(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].author.author > x)
+          case Title(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].title.title > x)
+          case Pages(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].pages.pages > x)
+          case IssueNumber(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].issueNumber.issueNumber > x)
+          case _ =>
+            println(s"Data type not supported")
+            List()
         }
-      case where: T => {
-        findOne(table)(where)
-      }
+      case "<" =>
+        where.last match {
+          case Author(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].author.author < x)
+          case Title(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].title.title < x)
+          case Pages(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].pages.pages < x)
+          case IssueNumber(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].issueNumber.issueNumber < x)
+          case _ =>
+            println(s"Data type not supported")
+            List()
+        }
+      case "=" =>
+        where.last match {
+          case Author(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].author.author equals x)
+          case Title(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].title.title equals x)
+          case Pages(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].pages.pages equals x)
+          case IssueNumber(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].issueNumber.issueNumber equals x)
+          case IsElectronic(x) =>
+            table.listRegisters.filter(_.asInstanceOf[LibraryItem].IsElectronic)
+          case _ =>
+            println(s"Data type not supported")
+            List()
+        }
+      case _ =>
+        println(s"No records found")
+        List()
     }
   }
 
   private def findOne(table: LibrarySQL[T])(register: T): List[T] = {
-    val selectRegister = this.listRegisters.filter(_ equals register)
-    if (selectRegister.size > 0) {
-      println(s"Record found : ${selectRegister}")
+    val selectRegister = table.listRegisters.filter(_ equals register)
+    if (selectRegister.nonEmpty) {
+      println(s"Record found : $selectRegister")
       selectRegister
     } else {
-      println(s"The item ${register} is not registered in the DB")
+      println(s"The item $register is not registered in the DB")
       List()
     }
   }
@@ -45,13 +86,13 @@ class LibrarySQL[T](var listRegisters: List[T]) {
     println("Update process...")
     val target: List[T] = findOne(table)(register)
     this.listRegisters.foreach(x => println(x))
-    if (this.listRegisters.filter(_ equals registerUpdate).size > 0) {
-      println(s"The item ${registerUpdate} already exists in the database")
+    if (table.listRegisters.exists(_ equals registerUpdate)) {
+      println(s"The item $registerUpdate already exists in the database")
     } else {
-      this.listRegisters = this.listRegisters.filter(!_.equals(target.head))
-      this.insertInto(table)(registerUpdate)
+      table.listRegisters = table.listRegisters.filter(!_.equals(target.head))
+      table.insertInto(table)(registerUpdate)
       println("Record successfully updated")
-      select(table)("*")
+      select(table)(List("*"))
     }
   }
 }
